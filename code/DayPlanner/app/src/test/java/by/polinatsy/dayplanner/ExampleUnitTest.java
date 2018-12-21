aa8efd626b13349955d43b89aceec57b179961ca
Polina Tsybukova <34811364+TsyPolina@users.noreply.github.com>
2018-12-21 04:22:30 +0300

    add Unit-test

--- code/DayPlanner/app/src/test/java/by/polinatsy/dayplanner/ExampleUnitTest.java
+++ code/DayPlanner/app/src/test/java/by/polinatsy/dayplanner/ExampleUnitTest.java
@@ -1,17 +1,38 @@
-package by.polinatsy.dayplanner;
+package by.polinatsy.dayplanner; 
 
-import org.junit.Test;
+import org.junit.Test; 
 
-import static org.junit.Assert.*;
+import by.polinatsy.dayplanner.model.*; 
+import static org.junit.Assert.*; 
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+public class ExampleUnitTest { 
+@Test 
+public void addition_isCorrect() { 
+assertEquals(4, 2 + 2); 
+} 
+@Test 
+public void task_test() { 
+ModelSeparator modelSeparator = new ModelSeparator(ModelSeparator.TYPE_FUTURE); 
+assertEquals(modelSeparator.isTask(), false); 
+modelSeparator.setType(ModelSeparator.TYPE_TOMORROW); 
+assertEquals(modelSeparator.getType(), ModelSeparator.TYPE_TOMORROW); 
+} 
+@Test 
+public void date_test() { 
+ModelTask model = new ModelTask(); 
+model.setDate(100); 
+assertEquals(model.getDate(), 100); 
+} 
+@Test 
+public void prioruty_test() { 
+ModelTask model = new ModelTask(); 
+model.setPriority(1); 
+assertEquals(model.getPriority(), 1); 
+} 
+@Test 
+public void status_test() { 
+ModelTask model = new ModelTask(); 
+model.setStatus(2); 
+assertEquals(model.getStatus(), 2); 
+} 
 }
